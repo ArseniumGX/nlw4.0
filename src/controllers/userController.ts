05228001a3b86f1134fdcae5eb915e84f0cc20b3
Jose Pereira Macedo <arseniumgx@gmail.com>
2021-02-25 14:52:47 -0300

    Aula 2 - Update 1

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,34 +4,53 @@ import { User } from '../models/User'
 
 class UserController
 {
+    /* Método cria a requisição */
     async create(req: Request, res: Response)
     {
         const { name, email } = req.body
         const usersRepository = getRepository(User)
 
-        // SELECT * FROM 'users' WHERE email = 'email'
+        /* 
+          SELECT * FROM 'users' WHERE email = 'email'
+          Variável armazena o valor do método
+        */
         const usersAlreadyExists = await usersRepository.findOne(
             {
                 email
             }
         )
 
+
+        /* Testa se usuário ja existe */
         if(usersAlreadyExists)
             return res.status(400).json({ 
                 error: "Usuário ja existe!"
-             })
+            })
 
+
+        /* Cria um novo repositório passando o nome e email da requisição do body */
         const user = usersRepository.create(
         {
             name, 
             email 
         })
 
-        try{ await usersRepository.save(user) } catch(e) { console.warn(`Error: ${e}`) }
+        /* Método salva as as informações no banco */
+        await usersRepository.save(user)
+        
+        /*
+        // Statement usado para testar um problema durando a gravação que estava acontecendo no save das informaçãos
+        try{
+            await usersRepository.save(user)
+        } catch(e){
+            console.warn(`Error: ${e}`)
+        }
+        */
 
-
+        /* envia a uma responsta em json das informações salvas em banco */
         return res.json(user)
     }
 }
+
 
 export { UserController }
--- src/database/migrations/1614210553583-CreateUsers.ts
+++ src/database/migrations/1614210553583-CreateUsers.ts
@@ -1,11 +1,20 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
+/*
+ *
+ *
+*/
+
 export class CreateUsers1614210553583 implements MigrationInterface {
 
+    /* Método sobe o tabela */
     public async up(queryRunner: QueryRunner): Promise<void> 
     {
         await queryRunner.createTable(new Table(
             {
+                /* Cria uma tabela users no banco com 4 colunas (id: uuid, name: varchar, email: varchar, createdAt: timestamp) 
+                   Os tipos de cada campo passado é o tipo que estabelecido pelo database, varia de acordo com o database escolhido
+                */
                 name: "users",
                 columns: [
                     {
@@ -24,6 +33,7 @@ export class CreateUsers1614210553583 implements MigrationInterface {
                         isNullable: false
                     },
                     {
+                        /* coluna createAt, assim como a id, o proprio database vai tratar essas informações de forma automatizada */
                         name: "createdAt",
                         type: "timestamp",
                         default: "now()"
@@ -33,6 +43,7 @@ export class CreateUsers1614210553583 implements MigrationInterface {
         )
     }
 
+    /* Método derruba a tabela criada do database */
     public async down(queryRunner: QueryRunner): Promise<void> 
     {
         await queryRunner.dropTable("user")
